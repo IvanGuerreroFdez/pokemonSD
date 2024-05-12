2c1a4a994dbdce8d6056a807fa383d74705f7f98
kaaxerd <kcharald@gmail.com>
2024-05-12 13:41:42 +0200

    cambios en character con estadisticas base para calcular mejor los evs

--- Game/AbstractFactoryGen1.java
+++ Game/AbstractFactoryGen1.java
@@ -2,42 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class AbstractFactoryGen1 implements AbstractFactory {
-    private File characterData = new File("./CharacterData.csv");
-    private File movePool = new File("./MovePool.csv");
+    private File characterData = new File("./CharacterData.csv"); // Access the CharacterData.csv
+    private File movePool = new File("./MovePool.csv"); // Access the MovePool.csv
     
-    public Character createGrass() {
+    public Character createGrass() { // Comments only made here, as everything is the same in all abstract files
         Character newCharacter = new Character();
         
         try {
             Scanner sc = new Scanner(characterData);
 
-            while(sc.hasNextLine()) {
+            while(sc.hasNextLine()) { // Gets the corresponding line
                 String line = sc.nextLine();
 
-                String[] pokeData = line.split(",|\\n"); // Splits if there is a comma
+                String[] pokeData = line.split(",|\\n"); // Stores the contents of the line in this array splitting if there is a comma
 
                 if(pokeData.length == 12) {
-                    for(String value : pokeData) {
+                    for(String value : pokeData) { // Test if the data is saved correctly
                         System.out.println(value);
                     }
                     
                     newCharacter = new Character(pokeData[0], pokeData[1], Integer.parseInt(pokeData[2]), Integer.parseInt(pokeData[3]), 
-                                                Integer.parseInt(pokeData[4]), Integer.parseInt(pokeData[5]), Integer.parseInt(pokeData[6]), Integer.parseInt(pokeData[7]));
+                                                Integer.parseInt(pokeData[4]), Integer.parseInt(pokeData[5]), Integer.parseInt(pokeData[6]), Integer.parseInt(pokeData[7])); // Stores the character
 
-                    newCharacter.moves[1] = new Move(pokeData[8], pokeData[1], Integer.parseInt(pokeData[9]), Integer.parseInt(pokeData[10]), Integer.parseInt(pokeData[11]));
+                    newCharacter.moves[1] = new Move(pokeData[8], pokeData[1], Integer.parseInt(pokeData[9]), Integer.parseInt(pokeData[10]), Integer.parseInt(pokeData[11])); // Stores the first move of the character
 
                     sc = new Scanner(movePool);
                     
-                    while(sc.hasNextLine()) {
+                    while(sc.hasNextLine()) { // Gets the corresponding line
                         line = sc.nextLine();
-                        String[] moveData = line.split(",|\\n"); // Splits if there is a comma
+                        String[] moveData = line.split(",|\\n"); // Stores the contents of the line in this array splitting if there is a comma
 
                         if(moveData.length == 5) {
                             /* for(String value : moveData) {
                                 System.out.println(value);
                             } */
 
-                            newCharacter.moves[2] = new Move(moveData[0], moveData[1], Integer.parseInt(moveData[2]), Integer.parseInt(moveData[3]), Integer.parseInt(moveData[4]));
+                            newCharacter.moves[2] = new Move(moveData[0], moveData[1], Integer.parseInt(moveData[2]), Integer.parseInt(moveData[3]), Integer.parseInt(moveData[4])); // Stores the second move of the character
                         }
                     }
                 }
--- Game/Character.java
+++ Game/Character.java
@@ -1,37 +1,47 @@
 public class Character {
     public String name, type;
+    private int baseHp, baseAtk, baseDef, baseSpA, baseSpD, baseSpe; 
     private int hp, atk, def, spa, spd, spe;
     public Move[] moves = new Move[3];
     public String status;
 
     public Character() {
-        // Assigns "Protect" as a default move for any Character
+        // Assigns "Protect" as a baseDefault move for any Character
         this.moves[0] = new Move("Protect", "Normal", 0, 100, +4);
     }
 
-    public Character(String name, String type, int hp, int atk, int def, int spa, int spd, int spe) {
+    public Character(String name, String type, int baseHp, int baseAtk, int baseDef, int baseSpA, int baseSpD, int baseSpe) {
         this.name = name;
         this.type = type;
 
         // Makes the corresponding calculations to change base stats from the .csv into real stats at level 50
-        this.hp = (((2 * hp) * 50) / 100) + 50 + 10; // (((2 * base) * lvl) / 100) + lvl + 10
-        this.atk = (((2 * atk) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
-        this.def = (((2 * def) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
-        this.spa = (((2 * spa) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
-        this.spd = (((2 * spd) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
-        this.spe = (((2 * spe) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
+        // Assigns final stats
+        this.hp = (((2 * baseHp) * 50) / 100) + 50 + 10; // (((2 * base) * lvl) / 100) + lvl + 10
+        this.atk = (((2 * baseAtk) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
+        this.def = (((2 * baseDef) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
+        this.spa = (((2 * baseSpA) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
+        this.spd = (((2 * baseSpD) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
+        this.spe = (((2 * baseSpe) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
 
-        // Assigns "Protect" as a default move for any Character
+        // Assigns base stats
+        this.baseHp = baseHp;
+        this.baseAtk = baseAtk;
+        this.baseDef = baseDef;
+        this.baseSpA = baseSpA;
+        this.baseSpD = baseSpD;
+        this.baseSpe = baseSpe;
+
+        // Assigns "Protect" as a baseDefault move for any Character
         this.moves[0] = new Move("Protect", "Normal", 0, 100, +4);
     }
 
     public void setIVs(int hpIV, int atkIV, int defIV, int spaIV, int spdIV, int speIV) {
         // Makes the corresponding changes with the IVs selected by the user
-        this.hp = (((2 * hp + hpIV) * 50) / 100) + 50 + 10; // (((2 * base) * lvl) / 100) + lvl + 10
-        this.atk = (((2 * atk + atkIV) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
-        this.def = (((2 * def + defIV) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
-        this.spa = (((2 * spa + spaIV) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
-        this.spd = (((2 * spd + spdIV) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
-        this.spe = (((2 * spe + speIV) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
+        this.hp = (((2 * this.baseHp + hpIV) * 50) / 100) + 50 + 10; // (((2 * base) * lvl) / 100) + lvl + 10
+        this.atk = (((2 * this.baseAtk + atkIV) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
+        this.def = (((2 * this.baseDef + defIV) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
+        this.spa = (((2 * this.baseSpA + spaIV) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
+        this.spd = (((2 * this.baseSpD + spdIV) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
+        this.spe = (((2 * this.baseSpe + speIV) * 50) / 100) + 5; // ((2 * base) * lvl / 100) + 5
     }
 }
