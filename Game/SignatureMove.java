99e271fc29d57ee85b6a7c452fea20f8ee18ceb7
kaaxerd <kcharald@gmail.com>
2024-05-12 14:14:14 +0200

    planteamiento inicial del patron strategy 2

--- Game/SignatureMove.java
+++ Game/SignatureMove.java
@@ -1,6 +1,15 @@
 public class SignatureMove implements Strategy {
-    public void action() { // Invokes the function to calculate and deal damage
-        DamageCalculator damage = new DamageCalculator();
-        damage.damageCalculator();
+    public void action() { 
+        // Handles turn order
+            // Check priority
+            // If no priority, check speed
+
+        // If enemy DOESNT protect, invoke damageCalculator taking in account the turn order
+        // If enemy protects, do nothing
+
+        if() {
+            DamageCalculator damage = new DamageCalculator();
+            damage.damageCalculator(); // Invokes the function to calculate and deal damage
+        }
     }
 }
--- Game/StatusMove.java
+++ Game/StatusMove.java
@@ -1,5 +1,10 @@
 public class StatusMove implements Strategy {
     public void action() {
-        
+        // Handles turn order
+            // Check priority
+            // If no priority, check speed
+
+        // If enemy DOESNT protect, invoke damageCalculator taking in account the turn order (ENEMY MAY ATTACK)
+        // If enemy protects, do nothing
     }
 }
